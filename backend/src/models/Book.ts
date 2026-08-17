38b820e9ff8053e1190d0d9e20bd05dd5772725b
rolandlazaro16 <rolandlazaro16@gmail.com>
2026-08-17 18:45:51 +0300

    fix: pre-save hook signature to enable auto-generating book numbers

--- backend/src/models/Book.ts
+++ backend/src/models/Book.ts
@@ -10,24 +10,19 @@ const bookSchema = new mongoose.Schema({
   bookNumber: { type: String, unique: true }
 });
 
-bookSchema.pre('save', async function(next) {
+bookSchema.pre('save', async function() {
   if (!this.bookNumber) {
-    try {
-      const BookModel = this.constructor as mongoose.Model<any>;
-      const lastBook = await BookModel.findOne({}, {}, { sort: { bookNumber: -1 } });
-      let nextNum = 1001;
-      if (lastBook && lastBook.bookNumber) {
-        const match = lastBook.bookNumber.match(/BF-(\d+)/);
-        if (match) {
-          nextNum = parseInt(match[1], 10) + 1;
-        }
+    const BookModel = this.constructor as mongoose.Model<any>;
+    const lastBook = await BookModel.findOne({}, {}, { sort: { bookNumber: -1 } });
+    let nextNum = 1001;
+    if (lastBook && lastBook.bookNumber) {
+      const match = lastBook.bookNumber.match(/BF-(\d+)/);
+      if (match) {
+        nextNum = parseInt(match[1], 10) + 1;
       }
-      this.bookNumber = `BF-${nextNum}`;
-    } catch (err: any) {
-      return next(err);
     }
+    this.bookNumber = `BF-${nextNum}`;
   }
-  next();
 });
 
 export const Book = mongoose.model('Book', bookSchema);
